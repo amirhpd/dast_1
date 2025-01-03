2d5b2d3bf65dce6e3293c9205f65875db3bdf544
Amir H. Pakdaman <ahpd1068@yahoo.com>
2025-01-03 16:24:13 +0100

    attempts to make plan visualization work. getCurrentState works, and topic display_planned_path gets values but plan is not shown in rviz.

--- src/moveit/src/interface_pose.cpp
+++ src/moveit/src/interface_pose.cpp
@@ -27,41 +27,35 @@ void move_robot_to_pose(
     float x, float y, float z, float roll, float pitch, float yaw)
 {
 
+    static const rclcpp::Logger LOGGER = rclcpp::get_logger("rclcpp");
     std::shared_ptr<rclcpp::Executor> executor_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-    
-    // auto mgi_options = moveit::planning_interface::MoveGroupInterface::Options("manipulator", "robot_description", "/");
-
-    // auto manipulator_move_group = moveit::planning_interface::MoveGroupInterface(node, mgi_options);
-    // auto manipulator_move_group = moveit::planning_interface::MoveGroupInterface(node, "manipulator");
-
-    // const moveit::core::JointModelGroup* joint_model_group =
-    //   manipulator_move_group.getCurrentState()->getJointModelGroup("manipulator");
 
     robot_model_loader::RobotModelLoader robot_model_loader(node);
     const moveit::core::RobotModelPtr& kinematic_model = robot_model_loader.getModel();
 
     moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(kinematic_model));
     robot_state->setToDefaultValues();
-    // const moveit::core::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("manipulator");
     auto mgi_options = moveit::planning_interface::MoveGroupInterface::Options("manipulator", "robot_description", "/");
     auto manipulator_move_group = moveit::planning_interface::MoveGroupInterface(node, mgi_options);
     manipulator_move_group.setPlannerId("RRTConnect");  // default
     manipulator_move_group.setPlanningTime(30.0);
-    // manipulator_move_group.setNumPlanningAttempts(10); 
-    // manipulator_move_group.setGoalTolerance(0.1);  
+    manipulator_move_group.setMaxVelocityScalingFactor(0.1);
+    manipulator_move_group.setMaxAccelerationScalingFactor(0.1);
+    manipulator_move_group.setNumPlanningAttempts(10); 
+    manipulator_move_group.setGoalPositionTolerance(0.001); 
 
     const moveit::core::JointModelGroup* joint_model_group =
       manipulator_move_group.getCurrentState()->getJointModelGroup("manipulator");
 
 
-    namespace rvt = rviz_visual_tools;
-    moveit_visual_tools::MoveItVisualTools visual_tools(node, "world", "display_planned_path", manipulator_move_group.getRobotModel());
-    visual_tools.deleteAllMarkers();
-    visual_tools.loadRemoteControl();
-    Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
-    text_pose.translation().z() = 1.75;
-    visual_tools.publishText(text_pose, "MoveGroupInterface Tutorial", rvt::WHITE, rvt::XLARGE);
-    visual_tools.trigger();
+    // namespace rvt = rviz_visual_tools;
+    // moveit_visual_tools::MoveItVisualTools visual_tools(node, "world", "display_planned_path", manipulator_move_group.getRobotModel());
+    // visual_tools.deleteAllMarkers();
+    // visual_tools.loadRemoteControl();
+    // Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+    // text_pose.translation().z() = 1.75;
+    // visual_tools.publishText(text_pose, "MoveGroupInterface Tutorial", rvt::WHITE, rvt::XLARGE);
+    // visual_tools.trigger();
 
     tf2::Quaternion quaternion;
     quaternion.setRPY(roll, pitch, yaw);
@@ -72,11 +66,11 @@ void move_robot_to_pose(
     target_pose.position.y = y;
     target_pose.position.z = z;
 
-    bool manipulator_at_goal = manipulator_move_group.setPoseTarget(target_pose);
+    bool manipulator_at_goal = manipulator_move_group.setPoseTarget(target_pose, "tip");
 
     if (!manipulator_at_goal)
     {
-        RCLCPP_WARN(rclcpp::get_logger("rclcpp"), "INVALID GOAL!");
+        RCLCPP_WARN(rclcpp::get_logger("interface_pose"), "INVALID GOAL!");
         return;
     }
 
@@ -84,28 +78,38 @@ void move_robot_to_pose(
     moveit::core::MoveItErrorCode plan_result = manipulator_move_group.plan(manipulator_plan);
 
     /* Visualize the plan in RViz */
-    visual_tools.publishAxisLabeled(target_pose, "pose");
-    visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
-    visual_tools.publishTrajectoryLine(manipulator_plan.trajectory_, joint_model_group);
+    auto display_publisher = node->create_publisher<moveit_msgs::msg::DisplayTrajectory>("/display_planned_path", 100);
+
+    // visual_tools.publishAxisLabeled(target_pose, "pose");
+    // visual_tools.publishText(text_pose, "Pose Goal", rvt::WHITE, rvt::XLARGE);
+    // visual_tools.publishTrajectoryLine(manipulator_plan.trajectory_, joint_model_group);
     // visual_tools.publishPath(manipulator_plan.trajectory_, rvt::LIME_GREEN, rvt::SMALL);
     // for (std::size_t i = 0; i < manipulator_plan.trajectory_.joint_trajectory.points.size(); ++i)
     // {
     //     // Visualize the target pose as a green sphere of size 0.03m
     //     visual_tools.publishSphere(manipulator_plan.trajectory_.joint_trajectory.points[i].positions, 0.03, rvt::GREEN);
     // }
-    visual_tools.trigger();
+    // visual_tools.trigger();
 
     if (plan_result == moveit::core::MoveItErrorCode::SUCCESS)
     {
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "MANIPULATOR PLAN SUCCEEDED!");
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), moveit::core::error_code_to_string(plan_result).c_str());
-        manipulator_move_group.move();
-        // manipulator_move_group.execute(manipulator_plan);  // both move() and execute() work
+
+        moveit_msgs::msg::DisplayTrajectory display_trajectory;
+        display_trajectory.model_id = "dast_1"; 
+        display_trajectory.trajectory_start = manipulator_plan.start_state_;
+        display_trajectory.trajectory.push_back(manipulator_plan.trajectory_);
+        display_publisher->publish(display_trajectory);
+        RCLCPP_INFO(rclcpp::get_logger("interface_pose"), "TRAJECTORY PUBLISHED!");
+
+        RCLCPP_INFO(rclcpp::get_logger("interface_pose"), "MANIPULATOR PLAN SUCCEEDED!");
+        RCLCPP_INFO(rclcpp::get_logger("interface_pose"), moveit::core::error_code_to_string(plan_result).c_str());
+        // manipulator_move_group.move();
+        manipulator_move_group.execute(manipulator_plan);  // both move() and execute() work
     }
     else
     {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "MANIPULATOR PLAN FAILED!");
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), moveit::core::error_code_to_string(plan_result).c_str());
+        RCLCPP_ERROR(rclcpp::get_logger("interface_pose"), "MANIPULATOR PLAN FAILED!");
+        RCLCPP_ERROR(rclcpp::get_logger("interface_pose"), moveit::core::error_code_to_string(plan_result).c_str());
         return;
     }
 }
@@ -122,11 +126,11 @@ int main(int argc, char **argv)
     executor.add_node(node);
     std::thread([&executor]() { executor.spin(); }).detach();
 
-    // if (argc != 7)
-    // {
-    //     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Usage: <x> <y> <z> <roll> <pitch> <yaw>");
-    //     return 1;
-    // }
+    if (argc != 7)
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Usage: <x> <y> <z> <roll> <pitch> <yaw>");
+        return 1;
+    }
 
     float x = std::stof(argv[1]);
     float y = std::stof(argv[2]);
@@ -135,19 +139,16 @@ int main(int argc, char **argv)
     float pitch = std::stof(argv[5]);
     float yaw = std::stof(argv[6]);
 
-    // std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("interface_pose");
-
-    // rclcpp::NodeOptions node_options;
-    // node_options.automatically_declare_parameters_from_overrides(true);
-    // auto node = rclcpp::Node::make_shared("interface_pose", node_options);
-
-    // rclcpp::executors::SingleThreadedExecutor executor;
-    // executor.add_node(node);
-    // std::thread([&executor]() { executor.spin(); }).detach();
-
-
     move_robot_to_pose(node, x, y, z, roll, pitch, yaw);
 
     rclcpp::shutdown();
     return 0;
 }
+
+
+// still does not show the planned path in rviz, but the topic /display_planned_path gets values and published.
+// to re-produce:
+// ros2 launch description gazebo.launch.py
+// ros2 launch moveit moveit.launch.py
+// ros2 run moveit interface_pose 2.094 -1.345 0.038 -3.053 0.000 1.000
+// ros2 topic echo /display_planned_path
